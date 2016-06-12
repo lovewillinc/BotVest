11f48ca40395fc757c98f312062a236249993406
Dustin Boyce <dustin@pheeva.com>
2016-06-12 14:54:44 -0500

    start

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
+views = {
+    welcome: function(ctrl) {
+        return m("h3", "Welcome")
+    },
+    homepage: function(ctrl) {
+        return m("h3", "Homepage")
+    },
+    viewAsset: function(ctrl) {
+        return m("h3", "View Asset info")
+    },
+    QRScan: function(ctrl) {
+        return m("h3", "QR Scan")
+    },
+    purchase: function(ctrl) {
+        return m("h3", "purchase")
+    },
+    portfolio: function(ctrl) {
+    	return m("h3", "portfolio")
+    }
+}
 var app = {
-	controller: function() {
-		var ctrl = {
-			foo: "bar"
-		}
-		console.log("Inside of the controller.")
-		return ctrl;
-	},
-	view: function(ctrl) {
-		return [
-			m("h3", "Baby Mithril")
-		]
-	}
+    controller: function() {
+        var self = this;
+        self.activeView = "welcome";
+        return self;
+    },
+    view: function(ctrl) {
+    	console.log("ctrl is:", ctrl);
+        return views[ctrl.activeView](ctrl)
+    }
 }
 m.mount(document.body, app)
