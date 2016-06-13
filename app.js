55d8e652017807ad097a4b045a9e1f1a2dbb8ee1
kobyhastings <koby_hastings1@mymail.eku.edu>
2016-06-13 10:56:23 -0500

    added assetDictionary

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+assetDictionary = require('./assetDictionary')
+console.log("assetDictionary", assetDictionary);
 Wallet = require('ethereumjs-wallet');
 m = require('mithril');
 Tx = require('ethereumjs-tx');
@@ -54,27 +56,6 @@ var app = {
             }
             localStorage.account = JSON.stringify(account);
             localStorage.ownedAssets = JSON.stringify([])
-            localStorage.assetDictionary = JSON.stringify([{
-                name: "Tesla Model S",
-                address: "0x73d61b6effc71243629aa3caedf496221f56a43f",
-                img: "http://www.blogcdn.com/www.autoblog.com/media/2012/09/01-2012-tesla-model-s-fd-1347336745.jpg"
-            }, {
-                name: "Ferrari Spider",
-                address: "0x0ca7d8ca1ac8c65989653f92114146d455245c83",
-                img: "http://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2015/07/28/102870926-150723_Ferrari488Spider_3-4AntAlto.1910x1000.jpg"
-            }, {
-                name: "Range Rover",
-                address: "0x27ef74865a164039b404c846fe6e2eaf91791630",
-                img: "http://www.landroverusa.com/Images/sc-ms2_293-191269_450x259.jpg?v=1"
-            }, {
-                name: "School Bus",
-                address: "0xe046bf95785303d83b04858655ffcd54bbf88e16",
-                img: "http://www.wenatcheeschools.org/student-support-services/images/1430399416118.jpg"
-            }, {
-                name: "Rolls Royce Limo",
-                address: "0x08fe194e8eba8df6fc2b613a80cbe9d57e9f174e",
-                img: "http://www.rollsroyceprice.com/wp-content/uploads/2015/03/Rolls-royce-limo.jpg"
-            }])
             web3Helper.fundAccount(account.address).then(function() {
                 self.accountBalance(100000)
                 self.changeView('homepage');
