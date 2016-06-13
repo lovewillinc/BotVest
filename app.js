1b28b96f23cfa4149a43d8d64e6c8151dde3f909
MichaelFioretti <michael.fioretti@uky.edu>
2016-06-12 23:35:52 -0500

    try catch add

--- app.js
+++ app.js
@@ -138,29 +138,37 @@ var app = {
             },
 
             self.test = function() {
-                cordova.plugins.barcodeScanner.scan(
-                    function(result) {
-                        if (!result.cancelled) {
-                            if (result.format == "QR_CODE") {
-                                navigator.notification.prompt("Please enter name of data", function(input) {
-                                    var name = input.input1;
-                                    var value = result.text;
+                alert("about to try it");
+                try {
 
-                                    var data = localStorage.getItem("LocalData");
-                                    console.log(data);
-                                    data = JSON.parse(data);
-                                    data[data.length] = [name, value];
 
-                                    localStorage.setItem("LocalData", JSON.stringify(data));
+                    cordova.plugins.barcodeScanner.scan(
+                        function(result) {
+                            if (!result.cancelled) {
+                                if (result.format == "QR_CODE") {
+                                    navigator.notification.prompt("Please enter name of data", function(input) {
+                                        var name = input.input1;
+                                        var value = result.text;
 
-                                    alert("Done");
-                                });
+                                        var data = localStorage.getItem("LocalData");
+                                        console.log(data);
+                                        data = JSON.parse(data);
+                                        data[data.length] = [name, value];
+
+                                        localStorage.setItem("LocalData", JSON.stringify(data));
+
+                                        alert("Done");
+                                    });
+                                }
                             }
-                        }
-                    },
-                    function(error) {
-                        alert("Scanning failed: " + error);
-                    })
+                        },
+                        function(error) {
+                            alert("Scanning failed: " + error);
+                        })
+                } catch (e) {
+                    alert("error");
+                    alert(e);
+                }
             }
 
         return self;
